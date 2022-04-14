69605813dbf090f93af9e961468875a2820068f8
elgringo32 <bknopke@gmail.com>
2022-04-13 22:15:02 -0700

    setting up fetch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
+const url = 'https://api.nasa.gov/planetary/apod?api_key=ER0thjIWiQXWLmev33zo4nD76cVZRY8iZLIIqf08'
+
+fetch(url) 
+    .then(res => res.json())
+    .then (data => {
+        console.log(data.explanation)
+        
+    })
+    .catch(err=> {
+        console.log(err)
+    })    
 
